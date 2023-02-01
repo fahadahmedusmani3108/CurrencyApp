242b06d697426c7cd9b982c2091df3dba592b7be
Fahad <fahad.ahmed@avanzasolutions.com>
2023-02-01 20:33:44 +0500

    Project Added

---
+++ CurrencyApp/CurrencyApp/CurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyAppApp.swift
+//  CurrencyApp
+//
+//  Created by Fahad Ahmed Usmani on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
